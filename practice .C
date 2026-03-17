6d8f7322ebd2b417c9177d970a5d0388967e9c77
Aman yadav <amanku12090@gmail.com>
2026-03-17 17:12:08 +0530

    Add C practice program

---
+++ practice .C
@@ -0,0 +1,75 @@
+// #include <stdio.h>
+// int main()
+// ---SUM of 'N' numbers ---
+    // {
+    //     int n;
+    //     printf("Enter number :");
+    //     scanf("%d \n", &n);
+
+    //     int sum = 0;
+    //     for (int i = 0; i <= n; i++){
+    //         sum = sum + i;
+    //     }
+    //     printf("sum is %d",sum);
+    //     return 0;
+    // };
+
+//      ---Table of N ---
+
+//     int n;
+//     printf("Enter number :");
+//     scanf("%d \n", &n);
+
+//     for (int i = 1; i <= 10; i++){
+//         printf("%d \n",i*n);
+
+//     }
+
+//     ----print even number ---
+
+//     int main(){
+//     int n;
+//     printf("Enter number :");
+//     scanf("%d \n", &n);
+//     printf("&d \n",n);
+
+//         if(n % 2 == 0){
+//         printf("given number is Even");
+//     }
+//     else{
+//         printf("given number is odd");
+//     }
+
+//     return 0;
+// }
+
+//   ---difine odd & even number---
+
+// int main(){
+//      for (int i=1; i <=100; i++){
+//         if(i % 2 ==0){
+//             printf("%d is Even number \n" , i);
+//         }
+//         else{
+//             printf("%d is odd number \n", i);
+//         }
+//     }
+     
+//     return 0;
+    
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter number :");
+//     scanf("%d \n",&n);
+
+//     if(n % 2==0){
+//         printf("given number is even");
+//     }
+//     else{
+//        printf("given number is odd");
+//     }
+
+//     return 0;
+// }
