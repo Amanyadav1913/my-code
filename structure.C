61e302176f108348e5bd006d1c568e4507cc686c
Aman yadav <amanku12090@gmail.com>
2026-03-25 06:45:45 +0530

    Add structure.C with student and employee examples

---
+++ structure.C
@@ -0,0 +1,80 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// typedef struct computerscincestudent {
+//     int roll;
+//  float cgpa;
+//  char name[100];
+// }coe;
+
+// int main(){
+//     coe s1;
+//     s1. roll =12;
+//     s1.cgpa =9.8;
+//     strcpy(s1.name ,"Aman");
+
+//     printf("s1.name is %s\n" ,s1.name);
+//     return 0;
+// }
+
+// struct student
+// {
+//     int roll;
+//     char name[100];
+//     float cgpa;
+// };
+
+// int main()
+// {
+//     struct student s1;
+//     s1.roll = 12;
+//     s1.cgpa = 8.9;
+//     strcpy(s1.name, "Aman");
+
+//     printf("s1.name =%s\n", s1.name);
+//     printf("s1.roll =%d\n", s1.roll);
+//     printf("s1.cgpa =%f\n", s1.cgpa);
+
+//     return 0;
+// }
+
+// struct employees{
+//     int id;
+//     char name[100];
+//     float salery;
+// };
+
+// int main(){
+//     struct employees emp[3];
+
+   
+//     for(int i=0; i<3; i++){
+
+//          printf("Employees %d detail \n",i+1);
+
+//         printf("id:\n");
+//         scanf("%d:\n",&emp[i].id);
+
+//         printf("name:\n");
+//         getchar();                              //buffer clear
+//         fgets(emp[i].name,100,stdin);
+
+//         printf("salery: \n");
+//         scanf("%f",&emp[0].salery);
+
+
+//     }
+//     printf("\n--Employees detail--\n");
+
+//     for(int i=0; i<3; i++){
+//         printf("\n Employees dtails =%d\n",i+1);
+
+//         printf("id: %d\n",emp[i].id);
+//          printf("name: %s\n",emp[i].name);
+//           printf("salery: %.2f\n",emp[i].salery);
+//     }
+
+ 
+
+//     return 0;
+// }
