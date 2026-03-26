33aad0c4f0f9020a17bdf18ebfde9f743b75ea19
Aman yadav <amanku12090@gmail.com>
2026-03-26 16:36:32 +0530

    Comment out if-else practice code
    Commented out various if-else statements for number checks.

---
+++ if else practice.c
@@ -0,0 +1,77 @@
+// #include<stdio.h>
+
+// int main(){
+    // int n;
+    // printf("Enter number:");
+    // scanf("%d",&n);
+
+    // if(n %2 ==0){
+    //     printf("given is Even number:%d",n);
+    // }else{
+    //     printf("given number is odd");
+    // }
+ 
+
+    // Largest number in given two number--
+
+    // int a,b;
+    // printf("Enter number:");
+    // scanf("%d %d",&a,&b);
+
+    // if(a>b && b<a){
+    //     printf("A is largest %d",a);
+    // } else{
+    // printf("B is largest number %d",b);
+    // }
+
+    // int n;
+    // printf("Enter number:");
+    // scanf("%d",&n);
+
+    // if(n>0){
+    //     printf("%d is positive number",n);
+    // }else if(n<0){
+    //     printf("%d is negative number",n);
+    // }else{
+    //     printf("%d is zero",n);
+    // }
+
+    // Find largest number in given three number--
+
+    // int a,b,c;
+    // printf("Enter number:");
+    // scanf("%d %d %d",&a,&b,&c);
+
+
+    // if(a>b && a>c){
+    //     printf("A is largest number %d",a);
+    // }else if(b>a && b>c){
+    //     printf("B is largest number %d",b);
+    // }else{
+    //     printf("c is largest number%d",c);
+    // }
+
+    // int n;
+    // printf("Enter number:");
+    // scanf("%d",&n);
+
+    // if(n%2== 0){
+    //     printf("%d is even number",n);
+
+    // }else {
+    //     printf("%d is odd number\n",n);
+    // }
+
+//     int n;
+//     printf("Enter number:");
+//     scanf("%d",&n);
+
+//     if(n>1 && n%1 ==0 || n%n==0){
+//         printf("%d is prime number",n);
+//     } else{
+//     printf("%d is natural number",n);
+//     }
+
+
+//      return 0;
+// }
